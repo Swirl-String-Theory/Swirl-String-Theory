3cb5f6677301dcb92af04e0970b8d29730774f76
Omar Iskandarani <info@omariskandarani.com>
2026-02-03 12:23:45 +0100

    Add initial Blender scripts for generating S40 winding factors and magnetic ring models

--- SST_Mathematical_Proof_Python/AngularApp/main.ts
+++
@@ -1,8 +0,0 @@
-
-import 'zone.js';
-import '@angular/compiler';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
-bootstrapApplication(AppComponent)
-  .catch(err => console.error('Angular bootstrap error:', err));
